4d352b39dabd34c21427111d45f0b428a3eae6cb
Your Name <you@example.com>
2025-03-21 19:23:25 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -83,7 +83,7 @@ posiciona en el elemento anterior.
 void eliminaElementos(List*L, int elem){
    int largo = get_size(L);
    int* dato = first(L);
-   for (int k = 0; k < largo; k++)
+   for (int k = 0; k <= largo; k++)
    {
       if (*dato == elem)
       {
